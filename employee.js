895fdc4a7abeb2531beb92fc1dc003c30dd35f0d
Raphael Nunes <46539496+rdefa001@users.noreply.github.com>
2020-01-01 18:29:51 +0000

    Update employee.js

--- employee.js
+++ employee.js
@@ -5,7 +5,7 @@ let Employee = {salary: 100000};
 let payGrades = {
   entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
   midLevel: { taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999 },
-  seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
+  seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary:  }
 };
 
 export function getCadre() {
